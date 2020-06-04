ae36837cce21971a65df238326109227855e2620
Dominic Vonk <dominic@vonk.io>
2020-06-04 08:07:52 +0200

    update

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $dotenv->load();
 
 $db = new Database('mysql:host='.getenv('DB_HOST').';port='.getenv('DB_PORT').';dbname='.getenv('DB_NAME'), getenv('DB_USER'), getenv('DB_PASS'));
 
-if (!isset($_GET['code']) || $_GET['code'] !== 'qvgK3vq0eGLPgCUD4EIL4pqzGVb0ruA1') {
+if (!isset($_GET['code']) || $_GET['code'] !== getenv('SELF_CODE')) {
     http_response_code(403);
     die();
 }
